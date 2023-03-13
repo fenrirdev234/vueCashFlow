2396391ea05b42690a6f579a357e94970bcb3c47
Ciro <ciroalejandro1@gmail.com>
2023-03-12 22:15:04 -0400

    init the project

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineAsyncComponent } from "vue";
+import SplashScreen from "./components/SplashScreen.vue";
+
+const Home = defineAsyncComponent(
+  () =>
+    new Promise((resolse) => {
+      setTimeout(() => {
+        resolse(import("./components/Home.vue"));
+      }, 2500);
+    })
+);
+</script>
+
+<template>
+  <Suspense>
+    <template #default>
+      <Home />
+    </template>
+    <template #fallback>
+      <SplashScreen />
+    </template>
+  </Suspense>
+</template>
+
+<style scoped>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+import SplashScreen from "./SplashScreen.vue";
+</script>
+
+<template>
+  <div>hi</div>
+</template>
